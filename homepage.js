e27e99cdc9829b78cc4fe6954474d18001fd3826
karalan <karajlangford@gmail.com>
2015-10-25 13:51:48 +0000

    Changes to way functions work
    Adjusted the way that clearing the local storage works, as for the
    purposes of the demo we can suppose that the comments are saved to an
    external DB and only cleared by an admin.

--- homepage.js
+++ homepage.js
@@ -1,59 +1,65 @@
- function saveComment(){
-	 
-	var cname = $('#namebox').val();
-	var ctext = $('#txt1').val();
-			
-		if (cname ==='CLEAR ALL'){
-				deleteComments();
-			} 
-			else 
-			{
-				if (cname === '') //if name box is left blank, the name is going to be changed into Anonymous
-				{
-					cname = 'Anonymous';
-					alert('As you have not entered your name, your comment will be recorded under \'Anonymous\'');
-				};
-			}
+"use strict";
+	function clearComment(){
+		if ($('#namebox').val () === 'CLEARALL'){
+			clearStorage();
+		};
+		$('#commbox').val('');
+	}; //changed clear function to this so that users can't clear comments from other users but admin can.
+	function saveComment(){
+		var cname = $('#namebox').val();
+		var ctext = $('#commbox').val();
+
+			//if (cname ==='CLEAR ALL'){
+			//		clearStorage();
+			//	}
+			//	else
+			//	{
+					if (cname === 'Your Name Here') //if name box is left blank, the name is going to be changed into Anonymous
+					{
+						cname = ('Anonymous');
+						alert('As you have not entered your name, your comment will be recorded under \'Anonymous\'');
+					};
+
 	var thour = new Date().getHours();
-		
-			if (thour<10)	//changing format for hours that there would be a 0 before the numbers
-			{
-					thour= "0"+thour;
-			}
-		
-	var tminutes = new Date().getMinutes();
-		
-			if (tminutes<10)	//changing format for minutes that there would be a 0 before the numbers
-			{
-					tminutes= "0"+tminutes;
-			}
-		
-	var tday = new Date().getDate();
-	var tmonth = new Date().getMonth();
-	var tyear = new Date().getFullYear();
-		
-	var cmtlist = '<p><class="cmtname" span><b>'+cname+' '+tday+'/'+(tmonth+1)+'/'+tyear+' '+thour+':'+tminutes+'</b></span></br>'+ctext+'</p>';
-	$('#cmtlist').append(cmtlist);
-	
-	var cmtlist2 = document.getElementById('cmtlist').innerHTML;
-	localStorage.setItem('comm',cmtlist2);
-	
-	clearComment();
-	};
 
-function loadStorage()
-	{
-	var cmtlist = localStorage.getItem('comm');
-	$('#cmtlist').append(cmtlist);
-	};
-function clearStorage()
-	{
-	localStorage.removeItem('comm');
-	window.location.reload();
-	};
+				if (thour<10)	//changing format for hours that there would be a 0 before the numbers
+				{
+						thour= "0"+thour;
+				}
 
-window.onload = function loadComment()
-	{
+		var tminutes = new Date().getMinutes();
+
+				if (tminutes<10)	//changing format for minutes that there would be a 0 before the numbers
+				{
+						tminutes= "0"+tminutes;
+				}
+
+		var tday = new Date().getDate();
+		var tmonth = new Date().getMonth();
+		var tyear = new Date().getFullYear();
+
+		var cmtlist = '<p><class="cmtname" span><b>'+cname+' '+tday+'/'+(tmonth+1)+'/'+tyear+' '+thour+':'+tminutes+'</b></span></br>'+ctext+'</p>';
+		$('#cmtlist').append(cmtlist);
+
+		var cmtlist2 = document.getElementById('cmtlist').innerHTML;
+		localStorage.setItem('comm',cmtlist2);
+
+		kclearComment();
+		};
+
+	function loadStorage()
+		{
 		var cmtlist = localStorage.getItem('comm');
 		$('#cmtlist').append(cmtlist);
-	};
+		};
+	function clearStorage()
+		{
+		localStorage.removeItem('comm');
+		window.location.reload();
+		};
+
+	window.onload = function loadComment()
+		{
+			var cmtlist = localStorage.getItem('comm');
+			$('#cmtlist').append(cmtlist);
+		};
