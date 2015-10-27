fa51d3e55e696ef912266ae1d3912a6e41223102
edgatas <edgarito42@gmail.com>
2015-10-27 10:57:24 +0000

    Changed Gallery header and CSS. Fixed saving bug
    Change gallery header to look the same as in the index page. Also added
    a bit of styles to gallery and change a few setting to the gallery
    itself. Commenting system now works and it saves the comment. Also,
    moved few lines to other position for better working purpose.

--- homepage.js
+++ homepage.js
@@ -1,46 +1,20 @@
 "use strict";
 	function clearComment(){
-		if ($('#namebox').val () === 'CLEARALL'){
-			clearStorage();
-		};
 		$('#commbox').val('');
+		$('#namebox').val('Your Name Here');
+		
 	}; //changed clear function to this so that users can't clear comments from other users but admin can.
 	function saveComment(){
 		var cname = $('#namebox').val();
 		var ctext = $('#commbox').val();
-		var hrate = $('#hotelrate').val();
-			
-		//This section will check which rating was given and how many start shuold be displayed in the comments	
-			if (hrate === '1')
-				{
-					var hRateStar = ('&#9734');
-					alert('1 star');
-				}
-				else{if (hrate === "2")
-				{
-					var hRateStar = ('&#9734&#9734');
-					alert('2 star');
-				}
-				else{if (hrate === "3")
-				{
-					var hRateStar = ('&#9734&#9734&#9734');
-					alert('3 star');
-				}
-				else{if (hrate === "4")
-				{
-					var hRateStar = ('&#9734&#9734&#9734&#9734');
-					alert('4 star');
-				}
-				else{
-					var hRateStar = ('&#9734&#9734&#9734&#9734&#9734');
-					alert('5 star');
-				}}}}
+		
+			//Cheching if CLEARALL was written, if yes, will clear all comments.
+			//Moved here, because CLEARALL comment was displayed just before clearing all the comments.
+			if ($('#namebox').val () === 'CLEARALL'){
+				clearStorage();
+			}
+			else{
 
-			//if (cname ==='CLEAR ALL'){
-			//		clearStorage();
-			//	}
-			//	else
-			//	{
 					if (cname === 'Your Name Here') //if name box is left blank, the name is going to be changed into Anonymous
 					{
 						cname = ('Anonymous');
@@ -65,39 +39,61 @@
 		var tmonth = new Date().getMonth();
 		var tyear = new Date().getFullYear();
 
+		
+		//This section is for taking the rate which user inputed and transfer it to text
+		var hrate = $('#hotelrate').val();
+			
+		//This section will check which rating was given and how many start shuold be displayed in the comments	
+			if (hrate === '1')
+				{
+					var hRateStar = ('&#9734');
+				}
+				else{if (hrate === "2")
+				{
+					var hRateStar = ('&#9734&#9734');
+				}
+				else{if (hrate === "3")
+				{
+					var hRateStar = ('&#9734&#9734&#9734');
+				}
+				else{if (hrate === "4")
+				{
+					var hRateStar = ('&#9734&#9734&#9734&#9734');
+				}
+				else{
+					var hRateStar = ('&#9734&#9734&#9734&#9734&#9734');
+				}}}}
+		
 		var cmtlist = '<p><class="cmtname" span><b>'+cname+' rated '+hRateStar+' stars '+tday+'/'+(tmonth+1)+'/'+tyear+' '+thour+':'+tminutes+'</b></span></br>'+ctext+'</p>';
 		$('#cmtlist').append(cmtlist);
 
+		//Assosiating new comments to second string for later use
 		var cmtlist2 = document.getElementById('cmtlist').innerHTML;
 		localStorage.setItem('comm',cmtlist2);
 
+		//Clearing input boxes from any text
 		clearComment();
-		};
-
-	function loadStorage()
-		{
-		var cmtlist = localStorage.getItem('comm');
-		$('#cmtlist').append(cmtlist);
-		};
+			};};
+		
+	//This function will clear the local storage from any data that are displayed in the comments.
 	function clearStorage()
 		{
 		localStorage.removeItem('comm');
 		window.location.reload();
 		};
 
-	window.onload = function loadComment()
-		{
-			var cmtlist = localStorage.getItem('comm');
-			$('#cmtlist').append(cmtlist);	
-		};
-
 	window.onload = function loadImageGallery()
 		{
+			//This part will make images in the thumbnail be displayed in a bigger picture above them when pressed on them.
 			$('#images_thumbnails a').click(function(){
 				var newImageSrc = $(this).attr('href');
 				$('#images_full img').attr({'src': newImageSrc });
 				return false;
 			});	
+			
+			//This part will load comment from local storage to display them when the page is refreshed
+			var cmtlist = localStorage.getItem('comm');
+			$('#cmtlist').append(cmtlist);	
 			
 		};
 		
