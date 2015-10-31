5df285c1983c1ef8ce1a90d5c09441a88f7b1ef8
karalan <karajlangford@gmail.com>
2015-10-31 12:54:06 +0000

    Maps Code Completed
    Maps Code is correct & Runs fine if run within HTML <head>, just need
    to get the DOM event listener to work properly so that it will load in
    page.

--- homepage.js
+++ homepage.js
@@ -2,13 +2,13 @@
 	function clearComment(){
 		$('#commbox').val('');
 		$('#namebox').val('Your Name Here');
-		
+
 	}; //changed clear function to this so that users can't clear comments from other users but admin can.
 	function saveComment(){
 		var cname = $('#namebox').val();
 		var ctext = $('#commbox').val();
-		
-			//Cheching if CLEARALL was written, if yes, will clear all comments.
+
+			//Checking if CLEARALL was written, if yes, will clear all comments.
 			//Moved here, because CLEARALL comment was displayed just before clearing all the comments.
 			if ($('#namebox').val () === 'CLEARALL'){
 				clearStorage();
@@ -39,11 +39,11 @@
 		var tmonth = new Date().getMonth();
 		var tyear = new Date().getFullYear();
 
-		
+
 		//This section is for taking the rate which user inputed and transfer it to text
 		var hrate = $('#hotelrate').val();
-			
-		//This section will check which rating was given and how many start shuold be displayed in the comments	
+
+		//This section will check which rating was given and how many start shuold be displayed in the comments
 			if (hrate === '1')
 				{
 					var hRateStar = ('&#9734');
@@ -63,7 +63,7 @@
 				else{
 					var hRateStar = ('&#9734&#9734&#9734&#9734&#9734');
 				}}}}
-				
+
 		var cmtlist = '<p><class="cmtname" span><b>'+cname+' rated '+hRateStar+' stars '+tday+'/'+(tmonth+1)+'/'+tyear+' '+thour+':'+tminutes+'</b></span></br>'+ctext+'</p>';
 		$('#cmtlist').append(cmtlist);
 
@@ -74,20 +74,20 @@
 		//Clearing input boxes from any text
 		clearComment();
 			};};
-		
+
 	//This function will clear the local storage from any data that are displayed in the comments.
 	function clearStorage()
 		{
 		localStorage.removeItem('comm');
 		window.location.reload();
 		};
-	
+
 	window.onload = callCMT;
 	function callCMT(){
 					//This part will load comment from local storage to display them when the page is refreshed
 			var cmtlist = localStorage.getItem('comm');
 			$('#cmtlist').append(cmtlist);
-			
+
 			$('#images_thumbnails a').click(function(){
 				var newImageSrc = $(this).attr('href');
 				$('#images_full img').attr({'src': newImageSrc });
@@ -95,16 +95,20 @@
 			})
 			alert('test');
 	}
-		
-	
-	//Scripts for map
-	function initialize() {
-		
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-          center: new google.maps.LatLng(44.5403, -78.5463),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
-      }	
+//Embedding of Google Map
+	 function initialize() {
+ 		var mapCanvas = document.getElementById('map'); 
+	  	var LatLng = new google.maps.LatLng(52.9398128, 0.4865628)
+		var mapOptions = { 
+			center: LatLng, 
+			zoom: 14, 
+			mapTypeId: google.maps.MapTypeId.ROADMAP 	}; 
+		var map = new google.maps.Map(mapCanvas, mapOptions) 
+		var marker = new google.maps.Marker({
+    		position: LatLng,
+    		map: map,
+    		title: 'The Golden Lion Hotel'
+  });
+  } 
+
+
