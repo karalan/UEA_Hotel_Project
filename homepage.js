ed491c8bd9f76bd10f8dc6698472c9a0ff89256b
karalan <karajlangford@gmail.com>
2015-11-01 22:30:52 +0000

    Expanding Divs
    Code added to enable expanding of Divs on Local Area page so user can
    just see info that they want rather than a wall of text.

--- homepage.js
+++ homepage.js
@@ -110,9 +110,23 @@
     		title: 'The Golden Lion Hotel'
   });
   } 
-//expanding in div
-	function expand(){
-		$('.expander').simpleexpand();
-}
+//Expanding Divs in Local Area page TODO: needs to close one when another is clicked
+		$(document).ready(function() {
+			$("#hotelbutton").click(function () {
+				$("#hoteltxt").slideToggle("slow");
+			});
+			$("#shopbutton").click(function () {
+				$("#shoptxt").slideToggle("slow");
+			});
+			$("#eatbutton").click(function () {
+				$("#eattxt").slideToggle("slow");
+			});
+			$("#barsbutton").click(function () {
+				$("#barstxt").slideToggle("slow");
+			});
+			$("#attbutton").click(function () {
+				$("#atttxt").slideToggle("slow");
+			});
+		});
 
 
