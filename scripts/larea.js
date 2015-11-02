2b46af69782101585bdd227faa1b13713b5c6735
karalan <karajlangford@gmail.com>
2015-11-02 22:45:45 +0000

    Div Closes when new one opened
    Means that only one div of info is open at once.

--- scripts/larea.js
+++ scripts/larea.js
@@ -1,18 +1,23 @@
-//Expanding Divs in Local Area page TODO: needs to close one when another is clicked
-		$(document).ready(function() {
-			$("#hotelbutton").click(function () {
-				$("#hoteltxt").slideToggle("slow");
-			});
-			$("#shopbutton").click(function () {
-				$("#shoptxt").slideToggle("slow");
-			});
-			$("#eatbutton").click(function () {
-				$("#eattxt").slideToggle("slow");
-			});
-			$("#barsbutton").click(function () {
-				$("#barstxt").slideToggle("slow");
-			});
-			$("#attbutton").click(function () {
-				$("#atttxt").slideToggle("slow");
-			});
+"use strict";
+	$(document).ready(function() {
+		$("#hotelbutton").click(function () {
+			$("#hoteltxt").slideToggle("slow");
+			$('.localcontent').not("#hoteltxt").slideUp();
 		});
+		$("#shopbutton").click(function () {
+			$("#shoptxt").slideToggle("slow");
+			$('.localcontent').not("#shoptxt").slideUp();
+		});
+		$("#eatbutton").click(function () {
+			$("#eattxt").slideToggle("slow");
+			$('.localcontent').not("#eattxt").slideUp();
+		});
+		$("#barsbutton").click(function () {
+			$("#barstxt").slideToggle("slow");
+			$('.localcontent').not("#barstxt").slideUp();
+		});
+		$("#attbutton").click(function () {
+			$("#atttxt").slideToggle("slow");
+			$('.localcontent').not("#atttxt").slideUp();
+		});
+	});
