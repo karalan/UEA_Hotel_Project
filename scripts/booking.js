88c8993324d4f01b69958400c174f56517a40d9d
karalan <karajlangford@gmail.com>
2015-11-02 23:01:04 +0000

    Formatting
    Formatting

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,17 +1,17 @@
 "use strict";
-function bnbinfo (){
-alert('B&B Rate includes any option from our breakfast menu within the price stated');
-					};
-function hbinfo (){
-alert('Half Board Rate includes any option from our breakfast menu within the' +
-    'price stated as well as a Three Course Evening Meal in our Hotel Restaurant');
-					};
-function redirect (){
-alert('Redirecting you to our booking partner.....');
-    window.location="http://www.booking.com";
-					};
-$(document).ready(function() {
-    $("#help1").click(bnbinfo);
-    $("#help2").click(hbinfo);
-});
+    function bnbinfo (){
+    alert('B&B Rate includes any option from our breakfast menu within the price stated','Help Info');
+    };
+    function hbinfo (){
+    alert('Half Board Rate includes any option from our breakfast menu within the' +
+        'price stated as well as a Three Course Evening Meal in our Hotel Restaurant');
+    };
+    function redirect (){
+    alert('Redirecting you to our booking partner.....');
+        window.location="http://www.booking.com";
+    };
+    $(document).ready(function() {
+        $("#help1").click(bnbinfo);
+        $("#help2").click(hbinfo);
+    });
 
