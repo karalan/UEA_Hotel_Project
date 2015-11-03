1430bd8b0de2926bce5b6956c2beea3940e32036
karalan <karajlangford@gmail.com>
2015-11-03 10:50:52 +0000

    Scripts to spoof sending
    Haven’t got these to work yet but is for show so just want it to clear
    all when ‘sent’ with an alert to say has sent .

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,1 +1,15 @@
 "use strict";
+function clearMessage(){
+    $('#messbox').val('');
+    };
+	function sendMessage() {
+        $('#fnamebox').val('');
+        $('#lnamebox').val('');
+        $('#emailbox').val('');
+        $('#telbox').val('');
+        $('#messbox').val('');
+        confirm('Your message has been sent')
+    };
+
+        //var totmessage = '<span>'+fname+lname+embox+tebox+mtext+'</span>';
+		//$('#cmtlist').append(cmtlist);
