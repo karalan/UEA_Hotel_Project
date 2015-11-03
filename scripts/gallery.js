42de68c10b817ad17a7a0cf565b4dbf4327ddaf6
karalan <karajlangford@gmail.com>
2015-11-03 22:30:55 +0000

    Added event handler to prevent large image from triggering default action of opening when clicked.

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,5 +1,5 @@
-	
-//This script is going to change the larges picture in the gallery by the one which is going to be pressed in the thumbnails
+"use strict";
+//This script is going to change the largest picture in the gallery by the one which is going to be pressed in the thumbnails
 	function gallery(){
 			$('#images_thumbnails a').click(function(){
 				var newImageSrc = $(this).attr('href');
@@ -7,9 +7,13 @@
 				return false;
 			})
 	}
-	
-//This script is checking if mobile device is being use. After that it will assign proper navigation style to the website.  
-
+//This stops the full image from opening if clicked
+$(document).ready(function() {
+	$('#images_full img').click(function(event){
+		event.preventDefault();
+	});
+});
+//This script is checking if mobile device is being use. After that it will assign proper navigation style to the website.
 	function testScreen(){
 		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
 			var mcontent = $('<tr><td><a href="index.html">Home</a></td></tr><tr><td><a href="booking.html">Booking</a></td></tr><tr><td><a href="gallery.html">Gallery</a></td></tr><tr><td><a href="localarea.html">Local Area</a></td></tr><tr><td><a href="directions.html">Find Us</a></td></tr><tr><td><a href="review.html">Reviews</a></td></tr><tr><td><a href="contact.html">Contact</a></td></tr>');
@@ -22,7 +26,6 @@
 		}
 
    }
-
 //This is a code by Simon Willison. It's allowing us to start a few function that start on page loading.
 	function addLoadEvent(func) {
 		var oldonload = window.onload;
