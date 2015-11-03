ded528f1535916ec67d87ce79729fdcef8926410
edgatas <edgarito42@gmail.com>
2015-11-03 20:32:19 +0000

    CSS loading fixed
    Now CSS loading as it should be. You can't see the [age without CSS

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,5 +1,6 @@
 	window.onload = gallery; //This script is going to change the larges picture in the gallery by the one which is going to be pressed in the thumbnails
 	function gallery(){
+			alert('gallery loading');
 			$('#images_thumbnails a').click(function(){
 				var newImageSrc = $(this).attr('href');
 				$('#images_full img').attr({'src': newImageSrc });
