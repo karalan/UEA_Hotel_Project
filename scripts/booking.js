99fc3a0ed95b5b6f4e9c9f4af19297614f33b52d
karalan <karajlangford@gmail.com>
2015-11-03 10:22:32 +0000

    Pop Ups changed to confirms
    For info used confirms, still alert for when redirecting off site.

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,9 +1,9 @@
 "use strict";
     function bnbinfo (){
-    alert('B&B Rate includes any option from our breakfast menu within the price stated','Help Info');
+    confirm('B&B Rate includes any option from our breakfast menu within the price stated','Help Info');
     };
     function hbinfo (){
-    alert('Half Board Rate includes any option from our breakfast menu within the' +
+    confirm('Half Board Rate includes any option from our breakfast menu within the' +
         'price stated as well as a Three Course Evening Meal in our Hotel Restaurant');
     };
     function redirect (){
