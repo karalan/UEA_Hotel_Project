e98ef70ea5392d01ca0e3c85b25ca830378679e4
edgatas <edgarito42@gmail.com>
2015-11-03 17:29:06 +0000

    Preparing for mobile style
    The navigation table is now created after detecting what device is being
    used.

---
+++ global.js
@@ -0,0 +1,17 @@
+window.onload = testScreen;
+	function testScreen(){
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+			var mcontent = $('<tr><td><a href="index.html">Home</a></td><td><a href="booking.html">Booking</a></td><td><a href="gallery.html">Gallery</a></td><td><a href="localarea.html">Local Area</a></td><td><a href="directions.html">Find Us</a></td><td><a href="review.html">Reviews</a></td><td><a href="contact.html">Contact</a></td></tr>');
+			$('#nav').append(mcontent);
+	}
+
+		else {
+			var dcontent = $('<tr><td><a href="index.html">Home</a></td><td><a href="booking.html">Booking</a></td><td><a href="gallery.html">Gallery</a></td><td><a href="localarea.html">Local Area</a></td><td><a href="directions.html">Find Us</a></td><td><a href="review.html">Reviews</a></td><td><a href="contact.html">Contact</a></td></tr>');
+			$('#nav').append(dcontent);
+	
+   }}
+   
+   
+   
+   
+   
--- homepage.js
+++
@@ -1,9 +0,0 @@
-window.onload = testScreen;
-	function testScreen(){
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-
-   alert('Mobile');
-}
-else {
-   alert('Desktop');
-   }}
